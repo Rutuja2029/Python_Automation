e0c77b7a91f9ed0e053a429b1917f2f2d9cc77ee
Rutuja2029 <rutujadeore1029@gmail.com>
2023-09-11 23:17:32 +0530

    for & while loop

---
+++ loops.py
@@ -0,0 +1,22 @@
+#For Loop
+Planet = "Earth"
+for i in Planet:
+    print(i)
+    
+print("rest of thr code")
+
+#for loop on tuple
+
+vaccines = ("Moderna","pfizer","sputnik")
+for vac in vaccines:
+    print(f"{vac} vaccine sprovides immuniazation")
+    
+# While Loop
+
+x = 0
+while x <= 10:
+    print("value of x is: ", x)
+    print("looping")
+    x += 1
+    
+print("end of the code")
