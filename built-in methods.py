820e79ae417964afa8cabf21c368d71f9dcf2e92
Rutuja2029 <rutujadeore1029@gmail.com>
2023-09-12 00:12:49 +0530

    built-in

---
+++ built-in methods.py
@@ -0,0 +1,18 @@
+#built-in methods/functions
+
+message = "corona vaccine is ready."
+print(message)
+print(message.capitalize())
+
+
+#to print all built in functions for dataype
+print(dir(""))
+print(dir([]))
+print(dir(()))
+print(dir({}))
+
+#======
+print(message.upper())
+Message = message.upper()
+print(message.lower())
+print(Message.islower())
