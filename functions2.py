0a6f1d8cef8e9e0f6c66b692990c0b85ae0755b5
Rutuja2029 <rutujadeore1029@gmail.com>
2023-09-12 12:35:17 +0530

    non key args

--- functions2.py
+++ functions2.py
@@ -1,4 +1,4 @@
-#key arguments
+#keyword arguments
 def vac_feedback(vac, efficacy):
     print(f"{vac} vaccine is having {efficacy} % efficacy")
     if(efficacy > 50 ) and (efficacy <= 75):
@@ -14,3 +14,19 @@ vac_feedback("pfizer", 80)
 vac_feedback("covax", 40)
 vac_feedback("covis", 95)
 vac_feedback(efficacy=100, vac="sputnik")
+
+print("+++++++++++++++++++")
+
+#variable length arguments - *args(non keyword arguments)
+def order_food(min_order, *args):
+    print(f"you have ordered :{min_order}")
+    #print(args)
+    for item in args:
+        print(f"you have ordered: {item}")
+    print("delivered")
+    
+order_food("Salad","Pizza","Biryani","Soup")
+
+print("==================")
+
+
