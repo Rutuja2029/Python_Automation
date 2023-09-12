04ec4724d99287585c49cc830d641c9dd2d57c56
Rutuja2029 <rutujadeore1029@gmail.com>
2023-09-12 11:22:24 +0530

    built-in commit 3

--- built-in methods.py
+++ built-in methods.py
@@ -16,3 +16,47 @@ print(message.upper())
 Message = message.upper()
 print(message.lower())
 print(Message.islower())
+ #-----------------
+print(message.find("ready"))
+print(message[18:22])
+print(message.find("not"))
+
+#===================
+sequence = ("192","168","40","90")
+print(".".join(sequence))
+print("/".join(sequence))
+print("***/&".join(sequence))
+
+#===================
+print("==============")
+
+moutains = ["Eve", "him","sah","Alps","K2","Mt Hood"]
+print(moutains)
+
+moutains.append("nilgiri")
+print(moutains)
+
+moutains.extend(["aravali","sat"])
+print(moutains)
+
+moutains.insert(2, "Mt Abu")
+print(moutains)
+
+moutains.pop()
+print(moutains)
+moutains.pop()
+print(moutains)
+moutains.pop()
+print(moutains)
+moutains.pop(2)
+print(moutains)
+
+
+#===================
+print("==============")
+
+comp_emp1 = {"Name":"Santa","Skill":"Blockchain","Code":1024}
+print(comp_emp1.keys())
+print(comp_emp1.values())
+comp_emp1.clear()
+print(comp_emp1)
